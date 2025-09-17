53def66f150209e4c2248bd561a11cdfdcab8e4a
Jianchang Liu <66350121+BlackTeaOff@users.noreply.github.com>
2025-09-17 06:35:02 +0000

    Uplode_Exp_2_Q2

--- EXP_2/main.c
+++ EXP_2/main.c
@@ -71,6 +71,62 @@ void increasingOrder(Node *head, int e) {
     p->next = new; // 值最大的情况
 }
 
+void reverse(Node *head) {
+    if (head == NULL) {
+        printf("该链表未初始化\n");
+        return;
+    }
+    else if (head->next == NULL) {
+        printf("该链表只有一个节点\n");
+        return;
+    }
+    Node *pre = NULL; // 可以直接让第一个节点(逆转后的最后一个节点next为NULL)
+    Node *cur = head->next;
+    Node *next = NULL;
+    while (cur != NULL) {
+        next = cur->next; // 保存目标(cur)的下一个节点
+        cur->next = pre; // 反向连接上一个节点
+        pre = cur; // 把节点都向后移动一位
+        cur = next;
+    }
+    head->next = pre; // 把原来的最后一个节点，与头节点相连(cur现在指向NULL)
+}
+
+/*
+void reverse(Node *head) {
+    if (head == NULL) {
+        printf("链表未初始化\n");
+        return;
+    }
+    Node *pre = head->next;
+    if (pre->next == NULL) {
+        printf("该链表只有一个节点(除头节点)\n");
+        return;
+    }
+    Node *this = pre->next;
+    Node *next;
+    if (this->next != NULL) { // 如果this->next == NULL，代表只有两个节点(除头节点)
+        next = this->next;
+    } else { // 只有两个节点的情况，pre和this，this->next == NULL，直接调换pre和this
+        this->next = pre;
+        pre->next = NULL;
+        head->next = this;
+        return;
+    }
+    pre->next = NULL;
+    while (next->next != NULL) { // 三个及三个以上节点的情况，直到next的下一位是NULL，即到了最后一个节点
+        this->next = pre; // 反向连接节点
+        pre = this; //所有指针后移一位
+        this = next;
+        next = next->next;
+    }
+    next->next = this;
+    this->next = pre;
+    head->next = next;
+    return;
+}
+*/
+
 void listNode(Node *head) {
     if (head == NULL) {
         printf("链表未初始化\n");
@@ -98,4 +154,14 @@ int main() {
     listNode(L);
     increasingOrder(L, 10);
     listNode(L);
+    reverse(L);
+    listNode(L);
+
+    Node *list = initList();
+    insertTail(list, 1);
+    insertTail(list, 2);
+    insertTail(list, 3);
+    listNode(list);
+    reverse(list);
+    listNode(list);
 }
